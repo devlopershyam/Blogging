0f942d1668eb12f4930124b05172fdd8e352e4ce
shyamverma <devlopershyam@gmail.com>
2025-07-09 00:14:02 +0530

    database connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello blog");
+// require('dotenv').config({path: './env'});
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+})
+
+connectDB(); 
